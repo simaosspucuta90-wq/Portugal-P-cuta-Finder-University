afa98622f5aff8afa9b4aa61717c6b76d45cb550
simaosspucuta90-wq <simaosspucuta90@gmail.com>
2026-07-15 18:02:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Esta linha abaixo é o segredo: ela diz ao servidor para servir ficheiros estáticos
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
