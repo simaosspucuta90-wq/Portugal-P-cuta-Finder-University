c8fcaa5454630ebb74bbad41bd9d2bfd15c3c012
simaosspucuta90-wq <simaosspucuta90@gmail.com>
2026-07-15 18:56:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-app.use(express.urlencoded({ extended: true }));
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Esta linha abaixo é o segredo: ela diz ao servidor para servir ficheiros estáticos
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Servidor a correr na porta ${port}`);
-});
 app.get('/contacto', (req, res) => {
     res.sendFile(path.join(__dirname, 'contacto.html'));
 });
+
 app.get('/universidades', (req, res) => {
     res.sendFile(path.join(__dirname, 'universidades.html'));
+});
+
+app.get('/formulario', (req, res) => {
+    res.sendFile(path.join(__dirname, 'formulario.html'));
+});
+
+app.post('/enviar-formulario', (req, res) => {
+    console.log(req.body);
+    res.send('<h1>Formulário recebido!</h1><a href="/">Voltar</a>');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor a correr na porta ${port}`);
 });
