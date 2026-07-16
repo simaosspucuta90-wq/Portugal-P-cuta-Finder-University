c81973a58429b50af1e1d198b9717b5f7953c3b6
simaosspucuta90-wq <simaosspucuta90@gmail.com>
2026-07-16 01:38:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,29 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// Rota para o site ler os dados
 app.get('/api/dados', (req, res) => {
-    const dados = JSON.parse(fs.readFileSync('dados.json', 'utf8'));
-    res.json(dados);
+    try {
+        const dados = JSON.parse(fs.readFileSync('dados.json', 'utf8'));
+        res.json(dados);
+    } catch (e) { res.json({ noticias: [] }); }
 });
 
-// Rota para publicar nova notícia
 app.post('/adicionar-noticia', (req, res) => {
-    const { titulo, descricao } = req.body;
     let dados = JSON.parse(fs.readFileSync('dados.json', 'utf8'));
-    dados.noticias.push({ titulo, descricao });
+    dados.noticias.push(req.body);
     fs.writeFileSync('dados.json', JSON.stringify(dados, null, 2));
-    res.send('Notícia publicada com sucesso! <a href="/admin">Voltar para o Painel</a>');
+    res.send('<h1>Publicado com sucesso!</h1><a href="/admin">Voltar ao Painel</a>');
 });
 
-// Rotas de Acesso
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'login.html')));
+app.get('/contacto.html', (req, res) => res.sendFile(path.join(__dirname, 'contacto.html')));
 app.post('/login', (req, res) => {
     if (req.body.password === "Simão123") res.sendFile(path.join(__dirname, 'painel.html'));
     else res.send('Senha incorreta! <a href="/admin">Tentar novamente</a>');
 });
-
-app.listen(port, () => console.log(`Servidor a correr na porta ${port}`));
+app.listen(process.env.PORT || 3000);
